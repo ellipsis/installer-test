86919e589409fa7030e3c7139c439929d19d6ade
G. Roggemans <g.roggemans@grog.be>
2016-03-07 15:27:23 +0100

    Initial implementation

---
+++ src/init.bash
@@ -0,0 +1,25 @@
+# init.bash
+#
+# Replaces the ellipsis init.bash file for running tests
+
+echo "Ellipsis path: $FINAL_ELLIPSIS_PATH"
+
+# Provide fake load function
+load() {
+    echo "load : $@"
+}
+
+# Provide fake fs.backup function
+fs.backup() {
+    echo "fs.backup : $@"
+}
+
+# Provide fake ellipsis.install function
+ellipsis.install() {
+    echo "ellipsis.install : $@"
+}
+
+# Provide fake os.platform function
+os.platform() {
+    :
+}
